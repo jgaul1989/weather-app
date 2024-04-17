197eba26396bbfd6a13e9def952e8561ff295818
Jonathan <jonathan.gaul1@gmail.com>
2024-04-17 14:53:16 -0400

    Added basic CSS to the web page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+cities = ['New York', 'San Francisco', 'Los Angeles']
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        cities.append(new_city)
+    return render_template('index.html', cities=cities)
 
 
 if __name__ == "__main__":
