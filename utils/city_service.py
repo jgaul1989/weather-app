337ec98cb19c6c6b90cb408ca7d74bd1d290dd66
Jonathan <jonathan.gaul1@gmail.com>
2024-04-17 19:02:02 -0400

    Created module to handle general helper functions for the application

---
+++ utils/city_service.py
@@ -0,0 +1,16 @@
+from models.city import City
+from .api_utils import get_weather_info_by_coordinates
+
+
+def create_cities(api_key):
+    cities = []
+    city_data = [
+        ("New York City", "40.714272", "-74.005966"),
+        ("San Francisco", "37.774929", "-122.419418"),
+        ("Houston", "29.763281", "-95.363274")
+    ]
+    for name, lat, lon in city_data:
+        state, temp = get_weather_info_by_coordinates(api_key, lat, lon)
+        city = City.create_city(name, state, temp)
+        cities.append(city)
+    return cities
