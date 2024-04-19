659c5a950b04d0558fb7b7c8b6bb37d298136a12
Jonathan <jonathan.gaul1@gmail.com>
2024-04-19 14:06:10 -0400

    Added basic client and server validation to require the user to enter a city that is at least 3 letters and zipcode that is at least 5 numbers. Flashes a message to the user if the input does not match the requirements

--- weather_app/models/city.py
+++ weather_app/models/city.py
@@ -5,12 +5,12 @@ from weather_app.extensions import db
 class City(db.Model):
     __tablename__ = 'city'
     city_id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    city_name: Mapped[str] = mapped_column(db.String(100))
+    city_name: Mapped[str] = mapped_column(db.String(100), nullable=False)
     weather_state: Mapped[str] = mapped_column(db.String(50))
     temp: Mapped[float] = mapped_column(db.Float)
     lat: Mapped[float] = mapped_column(db.Float)
     lon: Mapped[float] = mapped_column(db.Float)
-    zipcode: Mapped[str] = mapped_column(db.String(50))
+    zipcode: Mapped[str] = mapped_column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
         return f"City {self.city_name}"
